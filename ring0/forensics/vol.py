33e8615dd296cd4b9ad1d65e2752e76a67668cb0
chase1635321 <chasekanipe@gmail.com>
2019-06-13 11:07:01 -0400

    done

---
+++ ring0/forensics/vol.py
@@ -0,0 +1,8 @@
+# Automatically detects and selects profile
+# Can list and dump processes
+# Can list and dump files
+# Can hashdump
+# Each time, checks for flag with regular expression, also b64 version
+# Automatic strings with regex of img
+# Brute force all of these things
+# List command line commands
