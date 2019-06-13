ad92a5a16b7962880b974971f45454aa53b04ed6
chase1635321 <chasekanipe@gmail.com>
2019-06-13 11:28:41 -0400

    done

---
+++ ring0/forensics/pcap.py
@@ -0,0 +1,5 @@
+# Auto search with regex for flag or b64 encoded flag
+# Pretty dump pcap with tcpick -C -yP -r tcp_dump.pcap
+# Dump pcap with tcpdump -qns 0 -A -r blah.pcap
+# Auto dump all files, search for string
+
